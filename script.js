ff9fc82fc25fa081f71991806d42f03ed92a32c6
Abenezer <abitysank@gmail.com>
2025-04-08 18:15:54 -0400

    canvas setup with black square

---
+++ script.js
@@ -0,0 +1,7 @@
+let canvas = document.querySelector("#canvas");
+let ctx =  canvas.getContext("2d");
+let width = canvas.width;
+let height = canvas.height;
+
+ctx.fillStyle = "black";
+ctx.fillRect(0, 0, width, height)
