410b4309ec1be3bddc1e8b494f0d9e771ff1ad8f
Abenezer <abitysank@gmail.com>
2025-04-08 18:43:36 -0400

    convet the drawing into function to help with redrawing

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 let canvas = document.querySelector("#canvas");
-let ctx =  canvas.getContext("2d");
+let ctx = canvas.getContext("2d");
 let width = canvas.width;
 let height = canvas.height;
 
-ctx.fillStyle = "black";
-ctx.fillRect(0, 0, width, height)
+const BALL_SIZE = 5;
+let ballPosition = { x: 20, y: 30 };
+
+function draw() {
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, width, height);
+
+    ctx.fillStyle = "White";
+    ctx.fillRect(ballPosition.x, ballPosition.y, BALL_SIZE, BALL_SIZE);
+}
+
+draw();
