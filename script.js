3877e664989e9744507f99a45dd81f3deb9af831
Abenezer <abitysank@gmail.com>
2025-04-09 20:05:15 -0400

    stage 2

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ let height = canvas.height;
 const BALL_SIZE = 5;
 let ballPosition = { x: 20, y: 30 };
 
+let xSpeed = 4;
+let ySoeed = 2;
+
 function draw() {
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, width, height);
@@ -14,4 +17,32 @@ function draw() {
     ctx.fillRect(ballPosition.x, ballPosition.y, BALL_SIZE, BALL_SIZE);
 }
 
-draw();
+function update() {
+    ballPosition.x += xSpeed;
+    ballPosition.y += ySoeed;
+}
+
+function checkCollision() {
+    let ball = {
+        left: ballPosition.x,
+        right: ballPosition.x + BALL_SIZE,
+        top: ballPosition.y,
+        bottom: ballPosition.y + BALL_SIZE,
+    };
+
+    if (ball.left < 0 || ball.right > width) {
+        xSpeed = -xSpeed;
+    }
+
+    if (ball.top < 0 || ball.bottom > height) {
+        ySpeed = -ySpeed;
+    }
+}
+
+function gameLoop() {
+    draw();
+    update();
+    checkCollision();
+    setTimeout(gameLoop, 30); // Change setInterval to setTimeout
+}
+gameLoop();
